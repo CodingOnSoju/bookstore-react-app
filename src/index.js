cedeff59fdb8441ee9b1c1aab813a71feda2017b
Angelia TK <dreamcaster000@gmail.com>
2023-06-12 17:32:45 +0800

    Created necessary imports for bookstore app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Author from "./components/Author";
+import Message from "./components/Message";
+import Person from "./components/Person";
+import Title from "./components/Title";
+import Image from "./components/Image";
+import Book from "./components/Book";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Bookstore = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Bookstore />);
+
+export default Bookstore;
