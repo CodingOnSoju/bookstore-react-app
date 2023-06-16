d7278fa0e214478fc8993015ec7fb4d461174ca7
Angelia TK <dreamcaster000@gmail.com>
2023-06-16 11:12:50 +0800

    Organised code

---
+++ src/books.js
@@ -0,0 +1,39 @@
+//refactor the list of books
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "./images/book1.jpg",
+    id: 1,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Fourth Wing (The Empyrean, 1)",
+    img: "./images/book2.jpg",
+    id: 2,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "./images/book3.jpg",
+    id: 3,
+  },
+  {
+    author: "Jason Chaffetz",
+    title: "The Puppeteers",
+    img: "./images/book4.jpg",
+    id: 4,
+  },
+  {
+    author: "Stephen R.Covey",
+    title: "The Habits of Highly Effective People",
+    img: "./images/book5.jpg",
+    id: 5,
+  },
+  {
+    author: "Taylor Jenkins Reid",
+    title: "The Seven Husbands of Evelyn Hugo",
+    img: "./images/book6.jpg",
+    id: 6,
+  },
+];
